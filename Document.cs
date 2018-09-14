446e4c017162b74815932b44aeb30e7a3409a8f4
stevenstatham <42662297+stevenstatham@users.noreply.github.com>
2018-09-14 14:34:50 -0500

    Add files via upload

---
+++ Document.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name;
+			string fileName;
+			string fileContent;
+			string txtCatch = "";
+			int count;
+			string tryAgain = "yes";
+			StreamWriter thisFile = null;
+			Exception failure = null;
+
+			while (tryAgain == "yes")
+			{
+				Console.WriteLine("Document");
+				Console.WriteLine("What would you like to name this file?");
+				name = Console.ReadLine();
+
+				for (int counter = 1; counter <= 4; counter++)
+				{
+					txtCatch = txtCatch + name[name.Length - counter];
+				}
+				if (txtCatch == "txt.")
+				{
+					fileName = name;
+				}
+				else
+				{
+					fileName = name + ".txt";
+				}
+
+				Console.WriteLine("What would you like to include in the text file? Press ENTER when done.");
+				fileContent = Console.ReadLine();
+				count = fileContent.Length;
+
+				try
+				{
+					thisFile = File.CreateText(fileName);
+					thisFile.WriteLine(fileContent);
+				}
+				catch (IOException e)
+				{
+					failure = e;
+					Console.WriteLine("{0}", failure);
+					Console.WriteLine("type 'yes' in lowercase letters if you would like to try again.");
+					tryAgain = Console.ReadLine();
+				}
+				finally
+				{
+					if (thisFile != null)
+					{
+						thisFile.Close();
+						if (failure == null)
+						{
+							Console.WriteLine("{0} was successfully saved. The document contains {1} characters.", fileName, count);
+							tryAgain = "no";
+							Console.ReadLine();
+						}
+						failure = null;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Dog.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+	enum Gender
+	{
+		Male,
+		Female
+	}
+	class Dog
+	{
+		protected string name;
+		protected string owner;
+		protected int age;
+		protected Gender gender;
+		public Dog(string n, string o, int a, Gender g)
+		{
+			name = n;
+			owner = o;
+			age = a;
+			gender = g;
+		}
+		public void Bark(int n)
+		{
+			for (int count = 0; count < n; count++)
+			{
+				Console.WriteLine("Woof!");
+			}
+			Console.ReadLine();
+		}
+		public void GetTag()
+		{
+			string i;
+			string j;
+			string k;
+			if (gender == Gender.Male)
+			{
+				i = "His";
+				j = "He";
+			}
+			else
+			{
+				i = "Her";
+				j = "she";
+			}
+			if (age == 1)
+			{
+				k = "year";
+			}
+			else
+			{
+				k = "years";
+			}
+			Console.WriteLine("If lost, call {0}. {1} name is {2} and {3} is {4} {5} old.", owner, i, name, j, age, k);
+			Console.ReadLine();
+		}
+	}
+	class MakeDog
+	{
+		static void Main(string[] args)
+		{
+			Dog p = new Dog("Pluto", "Goofy", 8, Gender.Male);
+			p.Bark(5);
+			p.GetTag();
+
+			Dog s = new Dog("Skip", "Sarah", 1, Gender.Male);
+			s.Bark(8);
+			s.GetTag();
+
+			Dog n = new Dog("Navi", "Jake", 4, Gender.Female);
+			n.Bark(1);
+			n.GetTag();
+		}
+	}
+}
